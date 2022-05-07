bf61999417d58a853b0f1ae3c437c95d3e9f7811
Omar <omartsadek@gmail.com>
2022-05-07 19:14:54 -0400

    commit tree dec4d6e689021a24ee175e1b5ff2d064f80106c3

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const bookList = document.querySelector('#list')
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch(`http://localhost:3000/books`)
+    .then(resp => resp.json())     //con
+    .then(books => {
+        books.forEach((book) => {
+            bookHTML(book);
+        });
+    });
+});   //3//    
+
+const bookHTML = (book) => {
+    const li = document.createElement('li')      //for Eac
+    li.innerText = book.title
+    bookList.append(li);
+}
+// List Books
+
+// 
+//1. When the page loads
+//2. Get a list of books from http://localhost:3000/books and
+//3. Display their titles by creating a li
+//4. For each (iteration) book and adding each li to the ul#list element.
